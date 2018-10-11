c86a566164a8e6790611c4b2551a09139965f089
Julius Liebert <julius.liebert@joblocal.de>
2018-10-11 09:38:06 +0200

    adds entry point

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const aws = require('aws-sdk');
+const clear = require('clear');
+const { prompt } = require('inquirer');
+const { validateUrl } = require('./helper');
+const { handle } = require('./main');
+const { createClient } = require('./sqs');
+
+clear();
+
+prompt([
+  {
+    type: 'input',
+    name: 'awsRegion',
+    message: 'Enter your AWS region:',
+    default: 'eu-west-1',
+  },
+  {
+    type: 'input',
+    name: 'sourceQueueUrl',
+    message: 'Enter your source queue url:',
+    validate: validateUrl,
+  },
+  {
+    type: 'input',
+    name: 'targetQueueUrl',
+    message: 'Enter your target queue url:',
+    validate: validateUrl,
+  },
+]).then(async ({ awsRegion, sourceQueueUrl, targetQueueUrl }) => {
+  aws.config.update({ region: awsRegion });
+
+  const sqs = createClient(new aws.SQS());
+  let count;
+
+  try {
+    count = await handle({
+      sourceQueueUrl,
+      targetQueueUrl,
+      sqs,
+      prompt,
+    });
+  } catch (e) {
+    console.log(`! ${e.message}`);
+    process.exit(1);
+  }
+
+  console.log(`${count} messages move from ${sourceQueueUrl} to ${targetQueueUrl}!`);
+});
